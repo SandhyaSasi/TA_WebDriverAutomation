5d3de7fb86712f05eb8edf0f4d021234428cd3dd
Balaji G <gbalaji@thoughtworks.com>
2014-04-08 17:13:00 +0530

    [Christopher] Capture screenshots for failure tests

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -3,3 +3,25 @@ DatabaseCleaner.add_cleaner(:active_record,:connection => UserDB).strategy = :tr
 Before do |_|
   DatabaseCleaner.clean
 end
+
+After do |scenario|
+  $dirty_ui = true
+  if scenario.failed?
+    screenshot_name = screenshot_name(scenario)
+    save_screenshot screenshot_name
+    embed screenshot_name, 'image/png', 'Screenshot'
+    if ENV['PAUSE']
+      p 'Press enter to continue'
+      Readline.readline
+    end
+  end
+end
+
+def screenshot_name(scenario, extension = '.png')
+  scenario_line = scenario.backtrace_line.split(':in').first.split('/').last.gsub('.', '_').gsub(':', '_line_')
+  scenario_line + '_time_' + Time.now.strftime('%Y-%m-%d_%H-%M-%S') + extension
+end
+
+def save_screenshot(file_name)
+  Capybara.current_session.driver.save_screenshot 'features/output/' + file_name
+end
